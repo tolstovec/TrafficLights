76090a7128aca901c648e986d0616c1c5a061d23
Ilya Pokhodin <ilya.pokhodin@gmail.com>
2022-01-22 10:15:55 +0300

    Initial Commit

---
+++ TrafficLights/TrafficLightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightsApp.swift
+//  TrafficLights
+//
+//  Created by Ilya Pokhodin on 22.01.2022.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
